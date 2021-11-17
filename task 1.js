66ab90da91635aa8aa423460173461cb4f5d87e6
ArvinthSenthil <94062161+ArvinthSenthil@users.noreply.github.com>
2021-11-17 20:17:00 +0530

    Create task 1.js

---
+++ task 1.js
@@ -0,0 +1,30 @@
+let comp = [{
+ "Memory Summary" : [{
+     Maximum_capacity : "2048 MBytes",
+     Maximum_Memory_Module_Size : "4096 MBytes",
+     Memory_Slots : 4,
+     Error_Correction : "None",
+     DRAM_Frequnce : "199.0 MHz",
+     Memory_Timings : "3-3-3-8(CL-RCD-RP-RAS)"
+ }]
+ },  {
+        "Device Locator" : [{ 
+            Manufacturer : "Hyundai",
+            Part_Number : "HYMD264",
+            Serial_Number : "FFF7240",
+            Capacity : "512 MBytes",
+            Memory_Type : "DDR(PC3200)",
+            Speed : "200 MHz",
+            Supported_Frequencies : "133 MHz,166 MHz",
+            Memory_Timings : "2-2-2-6-0 at 133 MHz",
+            Data_Width : "64 bits",
+            Manufacturing_date : "2005,week 39",
+            EPP_SPD_Support : "NO",
+            XMP_SPD_Support : "NO"
+        }]
+        
+    }];
+ 
+ console.log(comp[0]);
+ console.log(comp[1]);
+ 
